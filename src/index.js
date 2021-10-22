1a08ca84ac8667a37315b46444048672d1128af2
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-22 10:51:08 +0200

    create a list of rebots

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import Card from "./components/card/card";
+import {robots} from "./Data/robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <h1>Hello world</h1>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+      <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
