8027936a8a102f70c676de680c4403dab2862e3e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-22 22:51:51 +0200

    add styles and font to our robots

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import CardList from "./components/card-list/card-list";
 import { robots } from "./Data/robots";
 import SearchBox from "./components/search-box/search-box";
 import { Component } from "react";
-import { render } from "@testing-library/react";
-
+import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,7 +24,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
